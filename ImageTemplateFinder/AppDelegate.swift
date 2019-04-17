c9e1d611991bdbb22082d9e778a3d6b2e6730be8
sszarek <stefan.szarek@grandparade.co.uk>
2019-04-17 22:27:18 +0200

    Initial Commit

---
+++ ImageTemplateFinder/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ImageTemplateFinder
+//
+//  Created by Stefan Szarek on 17/04/2019.
+//  Copyright © 2019 Stefan Szarek. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ImageTemplateFinder/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ImageTemplateFinder
+//
+//  Created by Stefan Szarek on 17/04/2019.
+//  Copyright © 2019 Stefan Szarek. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
