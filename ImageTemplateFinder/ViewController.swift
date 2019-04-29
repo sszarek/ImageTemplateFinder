c7af2157bdf2f11abdecfc433f376b0457880d3e
sszarek <stefan.szarek@gmail.com>
2019-04-29 15:32:05 +0200

    feat: reorganised user interface

--- ImageTemplateFinder/ViewController.swift
+++ ImageTemplateFinder/ViewController.swift
@@ -10,41 +10,22 @@ import Cocoa
 
 class ViewController: NSViewController {
     
-    @IBOutlet weak var templateImageView: NSImageCell!
-    @IBOutlet weak var imageImageView: NSImageView!
-    lazy var openImageView: ImageUploadView = {
-        let nib = NSNib(nibNamed: "ITFImageUploadView", bundle: nil)
-        var objects: NSArray?
-        nib?.instantiate(withOwner: self, topLevelObjects: &objects)
-        if let objects = objects,
-            let openImageView = objects.filter({$0 is ImageUploadView}).first as? ImageUploadView {
-            return openImageView
-        }
-        
-        return ImageUploadView()
-    }()
+    @IBOutlet weak var imageView: NSImageView!
+    @IBAction func openImageButtonClicked(_ sender: Any) {
+        guard let url = getImageUrl() else { return }
+        imageView.image = NSImage.init(contentsOf: url)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        self.view.addSubview(openImageView)
     }
 
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
         }
-    }
-
-    @IBAction func openTemplateButtonClicked(_ sender: Any) {
-        guard let url = getImageUrl() else { return }
-         templateImageView.image = NSImage.init(contentsOf: url)
-    }
-    
-    @IBAction func openImageButtonClicked(_ sender: Any) {
-        guard let url = getImageUrl() else { return }
-        imageImageView.image = NSImage.init(contentsOf: url)
     }
     
     func getImageUrl() -> URL? {
